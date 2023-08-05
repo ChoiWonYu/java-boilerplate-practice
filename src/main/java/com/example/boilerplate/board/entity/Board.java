b9be9394e73645e4af7a1abd001b4313c1304842
ChoiWonYu <phillip07290@naver.com>
2023-08-05 21:03:53 +0900

    feat: Board 湲곕낯 援ъ“

---
+++ src/main/java/com/example/boilerplate/board/entity/Board.java
@@ -0,0 +1,18 @@
+package com.example.boilerplate.board.entity;
+
+import com.example.boilerplate.member.entity.Member;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.ManyToOne;
+
+@Entity
+public class Board {
+
+
+
+}
