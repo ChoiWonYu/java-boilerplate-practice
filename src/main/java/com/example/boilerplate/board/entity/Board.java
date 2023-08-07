819515cd43ad23f5e30223396386d3040b2ad924
ChoiWonYu <phillip07290@naver.com>
2023-08-08 03:03:55 +0900

    feat: board, member 연관 관계 형성

--- src/main/java/com/example/boilerplate/board/entity/Board.java
+++ src/main/java/com/example/boilerplate/board/entity/Board.java
@@ -4,7 +4,6 @@ import com.example.boilerplate.member.entity.Member;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
@@ -25,8 +24,8 @@ public class Board {
 
   private String content;
 
-//  @ManyToOne(fetch = FetchType.LAZY)
-//  @JoinColumn(name="member_id")
-//  private Member member;
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name="member_id")
+  private Member member;
 
 }
--- src/main/java/com/example/boilerplate/member/entity/Member.java
+++ src/main/java/com/example/boilerplate/member/entity/Member.java
@@ -1,10 +1,14 @@
 package com.example.boilerplate.member.entity;
 
+import com.example.boilerplate.board.entity.Board;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import java.util.ArrayList;
+import java.util.List;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
@@ -26,6 +30,9 @@ public class Member {
   private String password;
 
   private String name;
+
+  @OneToMany(mappedBy = "member")
+  private List<Board> boards=new ArrayList<>();
 
   @Builder
   public Member(String email, String password, String name) {
