dbee5c69195ce87d0a150250654bbf419e132685
ChoiWonYu <phillip07290@naver.com>
2023-08-09 05:29:15 +0900

    feat: board 기본 응답 생성

--- src/main/java/com/example/boilerplate/board/controller/dto/BoardCommonResponse.java
+++ src/main/java/com/example/boilerplate/board/controller/dto/BoardCommonResponse.java
@@ -3,6 +3,7 @@ package com.example.boilerplate.board.controller.dto;
 import com.example.boilerplate.member.entity.Member;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonInclude.Include;
+import java.time.LocalDateTime;
 import java.util.UUID;
 import lombok.Builder;
 import lombok.Getter;
@@ -21,11 +22,14 @@ public class BoardCommonResponse {
 
   private String writer;
 
+  private LocalDateTime createdAt;
+
   @Builder
-  public BoardCommonResponse(UUID id,String title, String content, Member writer){
+  public BoardCommonResponse(UUID id,String title, String content, Member writer,LocalDateTime createdAt){
     this.id=id;
     this.title=title;
     this.writer=writer.getName();
     this.content=content;
+    this.createdAt=createdAt;
   }
 }
