fd6f9972efa9038daa53022c26f0f409dc555cf3
ChoiWonYu <phillip07290@naver.com>
2023-08-08 16:52:30 +0900

    feat: board common response dto 생성

--- src/main/java/com/example/boilerplate/board/controller/dto/BoardCommonResponse.java
+++ src/main/java/com/example/boilerplate/board/controller/dto/BoardCommonResponse.java
@@ -1,17 +1,17 @@
 package com.example.boilerplate.board.controller.dto;
 
+import com.example.boilerplate.member.entity.Member;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonInclude.Include;
 import java.util.UUID;
-import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
 @NoArgsConstructor
-@AllArgsConstructor
 @JsonInclude(Include.NON_NULL)
-public class BoardCreateResponse {
+public class BoardCommonResponse {
 
   private UUID id;
 
@@ -20,4 +20,12 @@ public class BoardCreateResponse {
   private String content;
 
   private String writer;
+
+  @Builder
+  public BoardCommonResponse(UUID id,String title, String content, Member writer){
+    this.id=id;
+    this.title=title;
+    this.writer=writer.getName();
+    this.content=content;
+  }
 }
