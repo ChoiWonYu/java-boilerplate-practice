3ea1fcb3f13c4c6e0d14c5e4654e4dbad1b45056
ChoiWonYu <phillip07290@naver.com>
2023-08-08 16:51:23 +0900

    feat: board builder와 todto 추가

--- src/main/java/com/example/boilerplate/board/entity/Board.java
+++ src/main/java/com/example/boilerplate/board/entity/Board.java
@@ -1,5 +1,7 @@
 package com.example.boilerplate.board.entity;
 
+import com.example.boilerplate.board.controller.dto.BoardCommonResponse;
+import com.example.boilerplate.board.controller.dto.BoardUpdateRequest;
 import com.example.boilerplate.member.entity.Member;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
@@ -8,11 +10,14 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import java.util.UUID;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import org.hibernate.annotations.UuidGenerator;
 
 @Entity
 @Getter
+@NoArgsConstructor
 public class Board {
 
   @Id
@@ -32,4 +37,26 @@ public class Board {
   @JoinColumn(name="member_id")
   private Member writer;
 
+
+  public void updateBoard(BoardUpdateRequest boardUpdateRequest){
+    this.title=boardUpdateRequest.getTitle();
+    this.content=boardUpdateRequest.getContent();
+  }
+
+  public BoardCommonResponse toDto(){
+    return BoardCommonResponse.builder()
+        .id(id)
+        .writer(writer)
+        .title(title)
+        .content(content)
+        .build();
+  }
+
+  @Builder
+  public Board(String title,String content,String imageUrl,Member member){
+    this.title=title;
+    this.content=content;
+    this.imageUrl=imageUrl;
+    this.writer=member;
+  }
 }
