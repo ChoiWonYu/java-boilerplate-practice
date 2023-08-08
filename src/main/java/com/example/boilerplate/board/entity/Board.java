53061439f6b8c7035d19e0e6bb3aeed79a243f1c
ChoiWonYu <phillip07290@naver.com>
2023-08-09 05:29:07 +0900

    feat: board에 생성 일자, 수정 일자 추가

--- src/main/java/com/example/boilerplate/board/entity/Board.java
+++ src/main/java/com/example/boilerplate/board/entity/Board.java
@@ -3,21 +3,28 @@ package com.example.boilerplate.board.entity;
 import com.example.boilerplate.board.controller.dto.BoardCommonResponse;
 import com.example.boilerplate.board.controller.dto.BoardUpdateRequest;
 import com.example.boilerplate.member.entity.Member;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import java.time.LocalDateTime;
 import java.util.UUID;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.hibernate.annotations.UuidGenerator;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 @Entity
 @Getter
 @NoArgsConstructor
+@EntityListeners(AuditingEntityListener.class)
 public class Board {
 
   @Id
@@ -37,6 +44,13 @@ public class Board {
   @JoinColumn(name="member_id")
   private Member writer;
 
+  @CreatedDate
+  @Column(updatable = false)
+  private LocalDateTime createdAt;
+
+  @LastModifiedDate
+  private LocalDateTime updatedAt;
+
 
   public void updateBoard(BoardUpdateRequest boardUpdateRequest){
     this.title=boardUpdateRequest.getTitle();
@@ -49,6 +63,7 @@ public class Board {
         .writer(writer)
         .title(title)
         .content(content)
+        .createdAt(createdAt)
         .build();
   }
 
