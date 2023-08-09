e6df0e67d6e6df7a2ae2497ef3c93d09195a2db0
ChoiWonYu <phillip07290@naver.com>
2023-08-09 16:19:17 +0900

    fix: S3 bucket 변수 인식 못하는 문제 해결

---
+++ src/main/java/com/example/boilerplate/aws/s3/S3Service.java
@@ -0,0 +1,34 @@
+package com.example.boilerplate.aws.s3;
+
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.model.ObjectMetadata;
+import java.io.IOException;
+import java.util.UUID;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+@Service
+@RequiredArgsConstructor
+public class S3Service {
+
+  private final AmazonS3 amazonS3;
+
+  @Value("${cloud.aws.s3.bucket}")
+  private String bucket;
+
+  public String uploadFile(MultipartFile multipartFile) throws IOException {
+    StringBuilder sb = new StringBuilder();
+    sb.append(UUID.randomUUID() + multipartFile.getOriginalFilename());
+    String fileName = sb.toString();
+
+    ObjectMetadata metaData = new ObjectMetadata();
+    metaData.setContentLength(multipartFile.getSize());
+    metaData.setContentType(multipartFile.getContentType());
+
+    amazonS3.putObject(bucket, fileName, multipartFile.getInputStream(), metaData);
+    return amazonS3.getUrl(bucket, fileName).toString();
+  }
+
+}
